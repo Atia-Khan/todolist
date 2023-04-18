23b1ab685d4faa0b652a60002209d2698ab6ab99
Atia Khan <atia.khan@xloopdigital.com>
2023-04-18 14:58:39 +0500

    todays work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import Todo from "./components/Todo";
 import "./App.css";
+import { useCallback } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  
+
+  const addTodos = (id,title,desc,date)=> {
+    const newTodos = {
+      id: Date.now(),title,desc,date
+      
+    }
+
+  } 
+
+  }
   return (
     <div>
       <Todo />
